68fb93cb1593397517efde5b780dc48a1286aaf9
ansariali <=>
2025-05-28 18:06:13 +0530

    Now working on Frontend functionality

---
+++ app/layout.jsx
@@ -0,0 +1,20 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["200", "300", "400", "500", "600", "700", "800", "900"],
+});
+
+export const metadata = {
+  title: "Backend",
+  description: "Learning Backend with Nextjs",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
